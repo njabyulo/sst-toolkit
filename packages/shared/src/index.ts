f4bdd57be5810987388e61de9de94a6078e50d26
Njabulo Majozi <services@njabulomajozi.com>
2025-11-10 21:16:27 +0200

    refactor(shared): remove utilities moved to core package
    - Remove state, relationships, and workflow utilities from shared
    - Update shared/src/index.ts to remove Utils export
    - Update shared/src/utils/index.ts to remove utility exports
    - Utilities are now consolidated in @sst-toolkit/core

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,4 @@
 export * as Types from "./types";
-export * as Utils from "./utils";
 export * as Schemas from "./schemas";
 export * as Constants from "./constants";
 
--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,4 +1,1 @@
-export * as State from "./state";
-export * as Relationships from "./relationships";
-export * as Workflow from "./workflow";
 
