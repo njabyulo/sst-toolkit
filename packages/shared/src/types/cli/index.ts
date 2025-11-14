a5eb5bd6dcc8e59280c05b9a135ce8d58cefa08a
Njabulo Majozi <services@njabulomajozi.com>
2025-11-14 06:51:29 +0200

    refactor(shared): update CLI types index to use namespace exports

--- packages/shared/src/types/cli/index.ts
+++ packages/shared/src/types/cli/index.ts
@@ -1,5 +1,5 @@
-export * as Resources from "./resources";
 export * as Commands from "./commands";
+export * as Resources from "./resources";
 export * as Config from "./config";
 export * as Logger from "./logger";
 export * as Plugins from "./plugins";
